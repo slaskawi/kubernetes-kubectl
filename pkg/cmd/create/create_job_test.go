ee4f9a309095c91a1ffcf61d2a3965f30b7822c4
Maciej Szulik <maszulik@redhat.com>
2020-05-13 12:59:49 +0200

    Set appropriate owner references when creating a job from cronjob
    Kubernetes-commit: b1a068b0fcfeac586df28a8b3d65db33dc3e1aa5

--- pkg/cmd/create/create_job_test.go
+++ pkg/cmd/create/create_job_test.go
@@ -163,9 +163,16 @@ func TestCreateJobFromCronJob(t *testing.T) {
 			expected: &batchv1.Job{
 				TypeMeta: metav1.TypeMeta{APIVersion: batchv1.SchemeGroupVersion.String(), Kind: "Job"},
 				ObjectMeta: metav1.ObjectMeta{
-					Name:            jobName,
-					Annotations:     map[string]string{"cronjob.kubernetes.io/instantiate": "manual"},
-					OwnerReferences: []metav1.OwnerReference{*metav1.NewControllerRef(cronJob, apps.SchemeGroupVersion.WithKind("CronJob"))},
+					Name:        jobName,
+					Annotations: map[string]string{"cronjob.kubernetes.io/instantiate": "manual"},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: batchv1beta1.SchemeGroupVersion.String(),
+							Kind:       cronJob.Kind,
+							Name:       cronJob.GetName(),
+							UID:        cronJob.GetUID(),
+						},
+					},
 				},
 				Spec: batchv1.JobSpec{
 					Template: corev1.PodTemplateSpec{
